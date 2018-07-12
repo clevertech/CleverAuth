0cf01a3668bc957ff7b3ab87eb68f8a49642ca65
Roman Podlinov <rpodlinov@gmail.com>
2018-07-12 14:32:49 +0300

    Made iDatabaseAdapter available

--- core/src/library.ts
+++ core/src/library.ts
@@ -1,6 +1,7 @@
 import Core from './core'
 import KnexAdapter from './database/knex'
 import MongoAdapter from './database/mongo'
+import IDatabaseAdapter from './database/adapter'
 import DefaultEmailService from './services/email'
 import TwilioSMSService from './services/sms/twilio'
 import Crypto from './utils/crypto'
@@ -12,6 +13,7 @@ export {
   Core,
   KnexAdapter,
   MongoAdapter,
+  IDatabaseAdapter,
   DefaultEmailService,
   DefaultPasswordsService,
   Crypto,
